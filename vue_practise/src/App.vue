db238c1caac1ec08614753f2f4f78730a325953b
HarishHec7700 <harish.hariharan7@gmail.com>
2023-02-23 18:01:14 +0530

    Vue JS

---
+++ vue_practise/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">{{ greet }} {{ pname }}
+    <a href="#" v-text="pname"></a>
+    
+    </div>   
+  <!-- here {{   }} is the mustache syntax the method of binding text using mustache syntax is called text interpolation  -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      greet:"Hello",
+      pname:"Kanni appan",
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
